9474a1c005484b8822fd722a3e9a378fc0e014d6
TilenOkretic <tilen.okretic@gmail.com>
2020-08-30 19:19:07 +0200

    fix before computer

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,5 +1,5 @@
 const monk = require('monk');
-const connection_string = 'localhost/messageboard';
+const connection_string = process.env.MONGODB_URI || 'localhost/messageboard';
 const db = monk(connection_string);
 
 module.exports = db;
